bf79ab3d075c2357276b79bb5c6a5f05fa49050b
giulianovarriale <giuliano.oca@gmail.com>
2017-04-01 11:22:16 -0300

    Refactoring Card component
    - Change to pure function since it is stateless for now
    - Add props validation (type checking and required props)
    - Add Add to list button

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,30 +1,50 @@
 import React, { Component } from 'react';
 
-class Card extends Component {
-  render() {
-    return (
-      <div className="card">
-        <div className="card-image">
-          <figure className="image is-4by2">
-            <img src={`https://image.tmdb.org/t/p/w400_and_h600_bestv2/${this.props.movie.poster_path}`}/>
-          </figure>
+const POST_PATH = 'https://image.tmdb.org/t/p/w400_and_h600_bestv2';
+
+let Card = (props) => {
+  let {title, overview, poster} = props;
+
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-4by2">
+          <img src={`${POST_PATH}/${poster}`}/>
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media">
+          <div className="media-content">
+            <p className="title is-4">
+              {title}
+            </p>
+          </div>
         </div>
-        <div className="card-content">
-          <div className="media">
-            <div className="media-content">
-              <p className="title is-4">
-                {this.props.movie.original_title}
+
+        <div className="content">
+          <div className="columns">
+            <div className="column">
+              <p>
+                {overview}
               </p>
             </div>
           </div>
-
-          <div className="content">
-            {this.props.movie.overview}
-          </div>
         </div>
       </div>
-    );
-  }
+
+      <footer className="card-footer">
+        <a className="card-footer-item">
+          Adicionar a lista
+        </a>
+      </footer>
+    </div>
+  );
 }
+
+Card.propTypes = {
+  title: React.PropTypes.string.isRequired,
+  overview: React.PropTypes.string.isRequired,
+  poster: React.PropTypes.string.isRequired
+};
 
 export default Card;
